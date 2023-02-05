8a18c8d910b4f0a636902403e7951e11a21b0a4d
sober <sobercoder@163.com>
2023-02-05 22:06:07 +0800

    stack

--- binarysearch.php
+++ binarysearch.php
@@ -18,7 +18,7 @@
 //echo $mid;die;
 /**
  * leetcode ---  704. 二分查找
- *
+ * 时间复杂度为O(logN)
  */
 class Solution704 {
 
@@ -70,7 +70,83 @@ class Solution704 {
         }
         return -1;
     }
+    // >= target  最左边的位置
+    function searchge($nums,$target) {
+        //左闭右闭
+        $n = count($nums);
+        $r = $n - 1;
+        $l = -1 ;
+        //满足条件执行
+        while ($r >= $l) {
+            $mid = $l + (($r - $l) >> 1);
+            // 说不定会没有等于的
+            if ($nums[$mid] >= $target) {
+                //往左边去查询
+                $r = $mid - 1;
+                //记录位置 万一找不到
+                $index = $mid;
+            } else if ($nums[$mid] < $target) {
+                $l = $mid + 1;
+            }
+        }
+        return $index;
+    }
+    // <= $target
+    public function searchle($nums, $target) {
+        $r = count($nums) - 1;
+        $l = 0;
+        $index = -1;
+        while ($r >= $l) {
+            $mid = $l + (($r - $l) >> 1);
+            if ($nums[$mid] <= $target) {
+                $l = $mid + 1;
+                $index = $mid;
+            } else if ($nums[$mid] > $target) {
+                $r = $mid - 1;
+            }
+        }
+        return $index;
+    }
+    //局部最小值
+    public function localMinival($nums) {
+        $n = count($nums);
+        // -1不存在
+        // 注意这里的边界条件
+        if ($n == 0) return -1;
+        if ($nums[0] < $nums[1]) return 0;
+        if ($nums[$n - 1] < $nums[$n - 2]) return $n - 1;
+
+        // 必定存在 但是不知道在哪里？？
+        $r = $n - 2;
+        $l = 1;
+        // 肯定会存在；
+        //满足条件 尼玛的别一直错！！！
+        while ($r >= $l) {
+            $mid = $l + (($r - $l) >> 1);
+            // d大于
+            if ($nums[$mid] > $nums[$mid + 1]) {
+//                $r = $mid - 1; //
+                $l = $mid + 1;
+            } else if ($nums[$mid] > $nums[$mid - 1]) {
+                // $mid - 1;//看一下怎么实现的；
+                $l = $mid - 1;
+            } else {
+                return $mid;
+            }
+        }
+    }
 }
+$arrceshi = [];
 $arr704 = [-1,0,3,5,9,12];
+//最左边界的问题>= target
+$arr704r = [1,2,3,3,3,3,4,5,5];
+//局部最小值的测试案例
+$arr7043 = [1,0,-1,1,0,1];
+
 $obj704 = new Solution704();
-echo $obj704->search($arr704,9);//4
+//echo $obj704->search($arr704,9);//4
+//echo $obj704->searchge($arr704r,3);//
+//echo $obj704->searchle($arr704r, 3);//
+// 设置一个初始值 不然要返回null
+//var_dump($obj704->searchle($arrceshi, 3));die; // 0 null
+var_dump($obj704->localMinival($arr7043));die;
--- stack.php
+++ stack.php
@@ -8,9 +8,12 @@
 
 
 /**
+ * php 的栈 就是 push 和 stack 来实现的一个栈；
+ * leetcode - 剑指 Offer 09. 用两个栈实现队列
  * Class CQueue
  * 两个栈实现一个队列；
  * 先入先出；FIFO;
+ * //两个栈实现一个队列
  */
 
 class CQueue {
@@ -29,22 +32,22 @@ class CQueue {
      * null = -1
      */
     function appendTail($value) {
-         if (!empty($this->stack2)) {
-             // 清空
-             $this->stack2 = [];
-         }
-
         array_push($this->stack1,$value);
     }
 
     /**
      * @return Integer
+     * //这里需要判断 stack2 和 stack1的情况 当不够的情况需要从 stack1里面取;****这个好好的学一下；
      */
     function deleteHead() {
-
-        $this->help();
-        if (empty($this->stack2)) return -1; // -1
-
+        if (empty($this->stack2))  {
+            // 把 stack1的元素 导入到 stack2
+            $this->help();
+            if (empty($this->stack2)) {
+                return -1;
+            }
+        }
+        //不为null 直接取；//直接删除
         return array_pop($this->stack2);
     }
 
@@ -53,6 +56,7 @@ class CQueue {
         //插入到stakc2
         // $this->>stack1 == null 那么就是
         while ($item = array_pop($this->stack1)) {
+            //
             array_push($this->stack2,$item);
         }
     }
@@ -66,3 +70,253 @@ $obj1->appendTail(4);
 echo $obj1->deleteHead();
 echo $obj1->deleteHead();
 echo $obj1->deleteHead();
+/**
+ *  155. 最小栈
+ * 我记得是维护两个栈
+ */
+
+class MinStack {
+    public $stack = [];
+    public $top = 0;
+    // 最小栈
+    public $minStack = [];
+    //指向的是下一个元素
+    public $minTop = 0;
+    /**
+     */
+    function __construct() {
+
+    }
+
+    /**
+     * @param Integer $val
+     * @return NULL
+     * //怎么获取栈顶元素？？？  php 怎么获取栈顶元素？ 维护一个栈顶指针吗？
+     * //z注意相同的也要插入；因为只有和栈顶相等的时候也要删除
+     */
+    function push($val) {
+        //先赋值 然后 ++
+        $this->stack[$this->top++] = $val;
+        // 只有维护也该值才能找到  栈顶
+        if (empty($this->minStack) || $val <= $this->minStack[$this->minTop - 1]) {
+            $this->minStack[$this->minTop] = $val;
+            //下一个元素要插入的index
+            $this->minTop++;
+        }
+    }
+
+    /**
+     * @return NULL
+     */
+    function pop() {
+        //
+        $res = array_pop($this->stack);
+        $this->top--;
+        //
+        if ($res == $this->minStack[$this->minTop -1]) {
+           array_pop($this->minStack) ;
+           $this->minTop--;
+        }
+    }
+
+    /**
+     * @return Integer
+     */
+    function top() {
+        // 栈顶；元素；； 所以必须要维护 top
+        if ($this->stack == null) return null;
+        return $this->stack[$this->minTop - 1];
+    }
+
+    /**
+     * @return Integer
+     */
+    function getMin() {
+        //最小值；
+        if (empty($this->minStack)) return 0;
+        return $this->minStack[$this->minTop - 1];
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * $obj = MinStack();
+ * $obj->push($val);
+ * $obj->pop();
+ * $ret_3 = $obj->top();
+ * $ret_4 = $obj->getMin();
+ */
+
+/**
+ * leetcode --  20. 有效的括号
+ * 有效括号
+ * 示例 2：
+输入：s = "()[]{}"
+输出：true
+ */
+// java 代码逻辑
+//class Solution {
+//    public boolean validateStackSequences(int[] pushed, int[] popped) {
+//        Deque<Integer> stack = new LinkedList<>();
+//        int n = pushed.length;
+//        for (int i = 0, j = 0; i < n; ++i) {
+//            stack.push(pushed[i]);
+//            while (!stack.isEmpty() && stack.peek() == popped[j]) {
+//                stack.pop();
+//                j++;
+//            }
+//}
+//return stack.isEmpty();
+//}
+//}
+class Solution {
+
+    /**
+     * @param String $s
+     * @return Boolean
+     *  最后栈是空那么就代表 括号是有效的；
+     * $s 就是一个字符串
+     *  字符串 就是一个字符数组 所以 可以通过 index索引来进行访问；
+     */
+    function isValid($s) {
+        $set = [')'=>'(',']'=>'[','}'=>'{'];
+        $stack = [];// 栈
+        $top = 0; //栈顶
+        $len = strlen($s);
+
+        for ($i = 0; $i < $len; $i++) {
+            //
+            if (!empty($stack) && $set[$s[$i]] == $stack[$top - 1]) {
+                array_pop($stack);
+                $top--;
+            } else {
+                array_push($stack, $s[$i]);
+                $top++;
+            }
+
+            // if (empty($stack)) {
+            //      array_push($stack, $s[$i]);
+            //      $top++;
+            // } else {
+            //     if ($set[$s[$i]] == $stack[$top - 1]) {
+            //         array_pop($stack);
+            //         $top--;
+            //     } else {
+            //         array_push($stack, $s[$i]);
+            //         $top++;
+            //     }
+            // }
+        }
+
+        return empty($stack) ? true : false;
+    }
+}
+
+/**
+ * 剑指 Offer 31. 栈的压入、弹出序列
+ * 栈的压入 和 弹出序列；
+ * php 不能记录当前栈的大小；所以需要维护一个top会方便一些 当然也可以使用count(n) -  1 来计算栈顶；
+ */
+
+class Solution31 {
+
+    /**
+     * @param Integer[] $pushed
+     * @param Integer[] $popped
+     * @return Boolean
+     * 数字均不相等；
+     */
+    function validateStackSequences($pushed, $popped) {
+        // 这种写法 要好好看一下呀；
+
+        $n = count($pushed);
+        $stack = [];
+
+        for ($i = 0, $j = 0; $i < $n; $i++) {
+            array_push($stack, $pushed[$i]);
+            //栈顶 怎么取求； 因为数组 count 要统计数组的长度  所以这里的时间复杂度是O(1); 类似于java中的peek当前元素；
+            // 注意这里不需要维护一个栈顶 指针 栈顶指针其实是 count($stack) - 1;
+            while (!empty($stack) && $stack[count($stack) - 1] == $popped[$j]) {
+                array_pop($stack);
+                $j++;
+            }
+        }
+        //  empty
+        return empty($stack) ? true : false;
+    }
+}
+/**
+ * 1472. 设计浏览器历史记录
+ * 网页的后退 和前进的实现
+ * 我感觉也是两个栈来实现的；
+ */
+
+class BrowserHistory {
+    public $forwardstack = [];
+    public $backstack = [];
+
+    /**
+     * @param String $homepage
+     */
+    //初始值？？？？？ //
+    function __construct($homepage) {
+        array_push($this->forwardstack, $homepage);
+    }
+
+    /**
+     * @param String $url
+     * @return NULL
+     */
+    function visit($url) {
+        // backforwd 要被清空？？？ 当插入新的元素的时候  back 要被清空？？？
+        array_push($this->forwardstack, $url);
+        // 直接给清空； 要清空；
+        $this->backstack = [];
+    }
+
+    /**
+     * @param Integer $steps
+     * @return String
+     */
+    function back($steps) {
+        //回退几步？？ // homepage 不能被改变不能被弹出；
+        //会留下一个homepage 家页面；
+        $n = count($this->forwardstack) - 1; //会剩下一个 //要剩下最后一个；；
+        $steps = $steps >= $n ? $n : $steps;
+        for ($i = 1; $i <= $steps; $i++) {
+            array_push($this->backstack, array_pop($this->forwardstack));
+        }
+        // return 栈顶元素
+        return $this->forwardstack[count($this->forwardstack) - 1];
+    }
+
+    /**
+     * @param Integer $steps
+     * @return String
+     */
+    function forward($steps) {
+        //回退几步？？ // homepage 不能被改变不能被弹出；
+        $n = count($this->backstack); //会剩下一个 // 元素的长度；
+        $steps = $steps >= $n ? $n : $steps;
+        for ($i = 1; $i <= $steps; $i++) {
+            array_push($this->forwardstack, array_pop($this->backstack));
+        }
+        // return 栈顶元素  栈顶元素
+        return $this->forwardstack[count($this->forwardstack) - 1];
+
+    }
+}
+
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * $obj = BrowserHistory($homepage);
+ * $obj->visit($url);
+ * $ret_2 = $obj->back($steps);
+ * $ret_3 = $obj->forward($steps);
+ */
+/**
+ * leetcode - 剑指 Offer II 041. 滑动窗口的平均值
+ *
+ */
